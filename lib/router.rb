fca9b60568f9ed0744c0ce367d88371d1e50a02c
Matthew Dunn <mshieldsdunn@mac.com>
2015-11-30 22:12:07 -0800

    Remove virtual file mapping

--- lib/router.rb
+++ lib/router.rb
@@ -1,5 +1,4 @@
 require 'rack'
-require 'yaml'
 
 module Shelf
   class Router
@@ -11,18 +10,9 @@ module Shelf
       # determine requested class#method(args)
       request = Rack::Request.new(env)
 
-      class_name  = request.path_info.split('/')[1]
-      method_name = request.path_info.split('/')[2]
+      class_name  = request.path_info.split('/')[1].capitalize
+      method_name = request.path_info.split('/')[2].downcase
       arguments   = request.params
-
-      # do we need to re-map to any custom routes?
-      config = YAML.load_file 'routes.yml'
-      if config['custom_routes'].key?("#{class_name}.#{method_name}")
-        custom_route = config['custom_routes']["#{class_name}.#{method_name}"]
-        custom_route = custom_route.split('.')
-        class_name   = custom_route[0]
-        method_name  = custom_route[1]
-      end
 
       # try to create class and send #method(arguments)
       begin
